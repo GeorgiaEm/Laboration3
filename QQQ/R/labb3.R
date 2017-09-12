5b8f519eb3cbd7a1a17ff0393443146e714e8ea1
Georgia Mushe <georgia.mushe@gmail.com>
2017-09-12 15:03:31 +0200

    Added comments and removed wiki_graph

--- QQQ/R/labb3.R
+++ QQQ/R/labb3.R
@@ -1,5 +1,5 @@
 ##### Lab 3 - Advanced R programming #####
-
+#Function 1
 euclidean <- function(a,b){
 
 gcd <- min(a,b)
@@ -16,16 +16,7 @@ return(gcd)
 
 }
 
-
-
-
-
-wiki_graph <-data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
-                        v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
-                        w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
-
-
-
+#Function 2
 dijkstra<-function(data,init_node){
   
   n <- length(unique(wiki_graph[,1]))
@@ -78,6 +69,5 @@ dijkstra<-function(data,init_node){
   
 }
 
-dijkstra(wiki_graph,3)
 
 
